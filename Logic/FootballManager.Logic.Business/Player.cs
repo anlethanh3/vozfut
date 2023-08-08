58cda8b8fedc64e65b65724712446950fbf076a1
TAMDEV <tamn0310@gmail.com>
2023-08-08 15:39:45 +0700

    test push

---
+++ Logic/FootballManager.Logic.Business/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace FootballManager.Logic.Business
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Elo { get; set; }
+        public BibColor BibColor { get; set; }
+    }
+
+    public enum BibColor
+    {
+        Orange,
+        Red,
+        Blue,
+        Green
+    }
+}
