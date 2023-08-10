5e7049ba50ffec929e8c006573a91b6357d64d4c
An Le <an.lethanh3@gmail.com>
2023-08-11 01:32:12 +0700

    - lock modal while login

--- Interface/FootballManager.Interface.Website/src/components/Login.tsx
+++ Interface/FootballManager.Interface.Website/src/components/Login.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
-import { Modal, Button, Alert } from 'react-bootstrap';
+import { Modal, Button, Alert, Spinner } from 'react-bootstrap';
 import { authenticate, profile } from '../providers/UserApiProvider'
 import { AxiosError, HttpStatusCode } from 'axios';
 import { ProfileProps } from '../reducers/UserReducer';
@@ -18,6 +18,7 @@ export default function Login(props: { show: boolean, onSubmit: (profile: Profil
     const [invalid, setInvalid] = useState({ email: false, password: false })
     const [error, setError] = useState<string | undefined>(undefined)
     const [isSuccess, setIsSuccess] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
     const onValid = () => {
         var check = { email: true, password: true }
         if (info.email) {
@@ -27,10 +28,14 @@ export default function Login(props: { show: boolean, onSubmit: (profile: Profil
             check = { ...check, password: false }
         }
         setInvalid({ ...check })
-        console.log(check)
-        // delay state in here ???
-        if (!check.email && !invalid.password) {
+        if (!check.email && !check.password) {
             onAuthenticate(info)
+        }
+    }
+
+    const onClickClose = () => {
+        if (!isLoading) {
+            onClose()
         }
     }
 
@@ -38,6 +43,7 @@ export default function Login(props: { show: boolean, onSubmit: (profile: Profil
         let abort = new AbortController()
         setIsSuccess(false)
         setError(undefined)
+        setIsLoading(true)
         authenticate({ signal: abort.signal, data: login })
             .then(response => {
                 console.log(response)
@@ -57,16 +63,20 @@ export default function Login(props: { show: boolean, onSubmit: (profile: Profil
                 console.log(profile)
                 setIsSuccess(true)
             })
-            .catch((ex: AxiosError) => {
-                setError("Login Failed!")
-            })
             .catch((ex: Error) => {
+                if (ex instanceof AxiosError && ex.response?.status === HttpStatusCode.Unauthorized) {
+                    setError("Login Failed!")
+                    return
+                }
                 setError(ex.message)
+            })
+            .finally(() => {
+                setIsLoading(false)
             })
     }
 
     return (
-        <Modal show={show} onHide={onClose}>
+        <Modal show={show} onHide={onClickClose} backdrop="static" keyboard={false}>
             <Modal.Header closeButton>
                 <Modal.Title>Login</Modal.Title>
             </Modal.Header>
@@ -88,8 +98,8 @@ export default function Login(props: { show: boolean, onSubmit: (profile: Profil
                         <FloatingLabel controlId="floatingName" label="Name">
                             <Form.Control type="text" required isInvalid={invalid.email}
                                 placeholder="Input email" value={info.email}
+                                disabled={isLoading}
                                 onChange={(e) => setInfo({ ...info, email: e.target.value })}
-                                onBlur={(e) => setInfo({ ...info, email: e.target.value })}
                             />
                             <Form.Control.Feedback type="invalid">Email is required.</Form.Control.Feedback>
                         </FloatingLabel>
@@ -97,16 +107,22 @@ export default function Login(props: { show: boolean, onSubmit: (profile: Profil
                     <Form.Group className="mb-3" controlId="addMember.Password">
                         <FloatingLabel controlId="floatingPassword" label="Password">
                             <Form.Control type="password" required isInvalid={invalid.password} placeholder="Input Password" value={info.password}
-                                onChange={(e) => setInfo({ ...info, password: e.target.value })}
-                                onBlur={(e) => setInfo({ ...info, email: e.target.value })} />
+                                disabled={isLoading}
+                                onChange={(e) => setInfo({ ...info, password: e.target.value })} />
                             <Form.Control.Feedback type="invalid">Password is required.</Form.Control.Feedback>
                         </FloatingLabel>
                     </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="primary" onClick={onValid}>
-                    Login
+                <Button disabled={isLoading} variant="primary" onClick={onValid}>
+                    {
+                        isLoading && <Spinner animation="border" variant="light" />
+                    }
+                    {
+                        !isLoading && `Login`
+                    }
+
                 </Button>
             </Modal.Footer>
         </Modal>
