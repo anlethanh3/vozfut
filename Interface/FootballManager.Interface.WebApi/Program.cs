90f5b28408a0c92f88369a8601bb443974271e59
An Le <an.lethanh3@gmail.com>
2023-08-14 17:31:52 +0700

    - support swagger on testing

--- Interface/FootballManager.Interface.WebApi/Program.cs
+++ Interface/FootballManager.Interface.WebApi/Program.cs
@@ -87,7 +87,7 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
