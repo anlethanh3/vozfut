07ec7f8ea9f1a4005fb75067ee35b3231bdefce7
An Le <an.lethanh3@gmail.com>
2023-09-08 10:56:42 +0700

    - roll sunday match

--- Testing/FooballManager.Logic.BusinessTest/TeamRivalRepositoryTest.cs
+++ Testing/FooballManager.Logic.BusinessTest/TeamRivalRepositoryTest.cs
@@ -98,5 +98,64 @@ namespace FooballManager.Logic.BusinessTest
             File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test2.json"), json);
             Assert.NotEmpty(json);
         }
+
+        [Fact]
+        public void TeamRivalJsonCNTest()
+        {
+            var teams = new List<TeamRival>
+            {
+                new TeamRival{
+                    Players= new List<Member>{
+                        new Member{Name = "Khoa Mái",Elo=3},
+                        new Member{Name = "Khải",Elo=4},
+                        new Member{Name = "Linh",Elo=4},
+                        new Member{Name = "Tiến Bịp",Elo=4},
+                        new Member{Name = "An Le",Elo=2},
+                        new Member{Name = "Đoàn",Elo=3},
+                        new Member{Name = "Vũ Huy",Elo=0},
+                    },
+                    EloSum=20,
+                },
+                new TeamRival{
+                    Players= new List<Member>{
+                        new Member{Name = "Hải Bánh",Elo=4},
+                        new Member{Name = "Nhân",Elo=4},
+                        new Member{Name = "Việt",Elo=3},
+                        new Member{Name = "Giang Trẻ",Elo=3},
+                        new Member{Name = "Dũng",Elo=3},
+                        new Member{Name = "Kuti",Elo=3},
+                        new Member{Name = "Duymax",Elo=0},
+                    },
+                    EloSum=20,
+                },
+                new TeamRival{
+                    Players= new List<Member>{
+                        new Member{Name = "Ryan",Elo=3},
+                        new Member{Name = "Cha Đạt",Elo=3},
+                        new Member{Name = "Phú Tê Giác",Elo=5},
+                        new Member{Name = "Thuận",Elo=3},
+                        new Member{Name = "Đông",Elo=3},
+                        new Member{Name = "Hoàng Mèo",Elo=3},
+                        new Member{Name = "Tony",Elo=0},
+                    },
+                    EloSum=20,
+                },
+                new TeamRival{
+                    Players= new List<Member>{
+                        new Member{Name = "Thắng Phạm",Elo=3},
+                        new Member{Name = "Huân",Elo=3},
+                        new Member{Name = "Bí Thư",Elo=3},
+                        new Member{Name = "Giang Già",Elo=2},
+                        new Member{Name = "Tuấn",Elo=4},
+                        new Member{Name = "Thầy",Elo=4},
+                        new Member{Name = "Cường",Elo=0},
+                    },
+                    EloSum=20,
+                }
+            };
+            var json = JsonConvert.SerializeObject(teams);
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test3.json"), json);
+            Assert.NotEmpty(json);
+        }
     }
 }
